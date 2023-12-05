3ea40fcbc0ac4a7fb1db2183119256382032a579
OOI JIA YI <ooijiayi93@gmail.com>
2023-12-05 17:43:32 +0800

    update default port to 8826

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :elixirChatApp, ElixirChatApp.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :elixirChatApp, ElixirChatAppWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 8826],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
